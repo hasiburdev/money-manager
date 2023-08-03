6d2906b274242c427c8780e5915b1181ae134ffa
Hasibur Rahman <hasiburdev@gmail.com>
2023-08-03 11:21:51 +0600

    Add config library

---
+++ apps/api/src/features/user.route.ts
@@ -0,0 +1,12 @@
+import Router from 'express'
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Successfully got users"
+    })
+})
+
+
+export const userRouter = router;
